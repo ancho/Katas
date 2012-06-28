c80110398de02b35c8cdfee97ef840a1fe93e3ad
Frank Becker <ancho@gmx.net>
2012-06-28 13:34:36 +0200

    literal numbers with delimiters

--- StringCalculator/src/main/java/base/StringCalculator.java
+++ StringCalculator/src/main/java/base/StringCalculator.java
@@ -2,11 +2,21 @@ package base;
 
 public class StringCalculator {
 
+	private static final String DEFAULT_DELIMITER = ",";
+
 	public int add(String expression) {
 		
 		if( expression.isEmpty() ){
 			return 0;
 		}
+		if( expression.contains(DEFAULT_DELIMITER) ){
+			String[] numberLiterals = expression.split(DEFAULT_DELIMITER);
+			return toInt( numberLiterals[0] ) + toInt( numberLiterals[1] );
+		}
+		return toInt(expression);
+	}
+
+	private int toInt(String expression) {
 		return Integer.parseInt(expression);
 	}
 
--- StringCalculator/src/test/java/base/StringCalculatorTest.java
+++ StringCalculator/src/test/java/base/StringCalculatorTest.java
@@ -3,20 +3,31 @@ package base;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class StringCalculatorTest {
 
+	private StringCalculator calculator;
+	
+	@Before
+	public void setUp(){
+		calculator = new StringCalculator();
+	}
+	
 	@Test
 	public void empty_string_should_evaluate_to_zero() {
-		StringCalculator calculator = new StringCalculator();
 		assertThat( calculator.add(""), is(0) );
 	}
 	
 	@Test
 	public void single_literal_number_should_avaluate_ist_value() throws Exception {
-		StringCalculator calculator = new StringCalculator();
 		assertThat( calculator.add("42"), is(42) );
+	}
+	
+	@Test
+	public void literal_numbers_with_delimiter_should_evaluate_to_sum() throws Exception {
+		assertThat( calculator.add( "1,2" ), is(3) );
 	}
 
 }
