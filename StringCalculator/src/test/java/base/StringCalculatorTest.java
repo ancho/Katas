4aadaefec9338eaa47a55e10fbae3f3b3a8106c2
Frank Becker <ancho@gmx.net>
2012-06-28 13:14:47 +0200

    new base-package
    Empty String Test finished.

--- StringCalculator/src/test/java/base/StringCalculatorTest.java
+++ StringCalculator/src/test/java/base/StringCalculatorTest.java
@@ -1,17 +1,16 @@
+package base;
+
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
 
 import org.junit.Test;
-
-
 
 public class StringCalculatorTest {
 
 	@Test
 	public void empty_string_should_evaluate_to_zero() {
 		StringCalculator calculator = new StringCalculator();
-		
-		assertThat( calculator.add( "" ), is(0) );
+		assertThat( calculator.add(""), is(0) );
 	}
 
 }
