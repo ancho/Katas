7aeb7a00ff6f3a318725be4b60b8fa64708a1836
Frank Becker <ancho@gmx.net>
2012-06-29 11:30:07 +0200

    single number literal returns its value

--- StringCalculator/src/main/java/base/StringCalculator.java
+++ StringCalculator/src/main/java/base/StringCalculator.java
@@ -3,8 +3,9 @@ package base;
 public class StringCalculator {
 
 	public int add(String expression) {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		if( expression.isEmpty() ) return 0;
+		return Integer.parseInt(expression);
 	}
 
 }
--- StringCalculator/src/test/java/base/StringCalculatorTest.java
+++ StringCalculator/src/test/java/base/StringCalculatorTest.java
@@ -1,5 +1,6 @@
 package base;
 
+import static org.junit.Assert.*;
 import static org.junit.Assert.assertThat;
 import static org.hamcrest.CoreMatchers.*;
 
@@ -9,10 +10,15 @@ public class StringCalculatorTest {
 
 	@Test
 	public void empty_string_should_retrun_zero() {
-
 		StringCalculator calculator = new StringCalculator();
 		assertThat( calculator.add(""), is(0));
-		
 	}
+
+	@Test
+	public void single_number_literal_returns_its_value() throws Exception {
+		StringCalculator calculator = new StringCalculator();
+		assertThat( calculator.add("42"), is(42));
+	}
+	
 
 }
