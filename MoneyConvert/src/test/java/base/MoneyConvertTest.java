73b7238dabd60995c323f07121b84b9755c4d00a
Frank Becker <ancho@gmx.net>
2012-12-02 19:32:48 +0100

    one test for single-digit convert

--- MoneyConvert/src/test/java/base/MoneyConvertTest.java
+++ MoneyConvert/src/test/java/base/MoneyConvertTest.java
@@ -16,19 +16,19 @@ public class MoneyConvertTest {
 		mc = new MoneyConvert();
 	}
 
+	
 	@Test
-	public void convert_0_to_EmptyString() {
+	public void singleDigitsConvert() throws Exception {
 		assertThat(mc.convert(0), is(""));
-	}
-	
-	@Test
-	public void convert_1_to_one() throws Exception {
 		assertThat(mc.convert(1), is("one"));
-	}
-	
-	@Test
-	public void convert_2_to_two() throws Exception {
 		assertThat(mc.convert(2), is("two"));
+		assertThat(mc.convert(3), is("three"));
+		assertThat(mc.convert(4), is("four"));
+		assertThat(mc.convert(5), is("five"));
+		assertThat(mc.convert(6), is("six"));
+		assertThat(mc.convert(7), is("seven"));
+		assertThat(mc.convert(8), is("eight"));
+		assertThat(mc.convert(9), is("nine"));
 	}
 
 }
