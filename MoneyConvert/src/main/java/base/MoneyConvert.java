ee54c121888e5d12948e43dd870d14b5f2663fb2
Frank Becker <ancho@gmx.net>
2012-12-02 19:30:46 +0100

    refactoring

--- MoneyConvert/src/main/java/base/MoneyConvert.java
+++ MoneyConvert/src/main/java/base/MoneyConvert.java
@@ -1,15 +1,31 @@
 package base;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MoneyConvert {
 
+	private static Map<Integer, String> dictionary = new HashMap<Integer, String>();
+	
+	static {
+		dictionary.put(0, "");
+		dictionary.put(1, "one");
+		dictionary.put(2, "two");
+		dictionary.put(3, "three");
+		dictionary.put(4, "four");
+		dictionary.put(5, "five");
+		dictionary.put(6, "six");
+		dictionary.put(7, "seven");
+		dictionary.put(8, "eight");
+		dictionary.put(9, "nine");
+		
+	}
 	public String convert(int amount) {
-		if ( amount == 1 ){
-			return "one";
-		}
-		else if ( amount == 2 ) {
-			return "two";
-		}
-		return "";
+		return convertSingleDigit(amount);
+	}
+	
+	private String convertSingleDigit(int amount) {
+		return dictionary.get(amount);
 	}
 
 }
